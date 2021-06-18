9bd1f14ed9dcb2a74017f38594b6d95181b28a2a
Christopher Hewett <chewett@hotmail.co.uk>
2021-06-18 11:24:00 +0100

    Commit Day 14 into memory

---
+++ src/test/java/net/chewett/adventofcode/aoc2020/problems/Day14Test.java
@@ -0,0 +1,70 @@
+package net.chewett.adventofcode.aoc2020.problems;
+
+import net.chewett.adventofcode.helpers.ProblemLoader;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day14Test {
+
+    /**
+     * Test Day 14 part one with the example input (with the known result)
+     */
+    @Test
+    public void testExampleInputPartOne() {
+        List<String> instructions = new ArrayList<>();
+        instructions.add("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+        instructions.add("mem[8] = 11");
+        instructions.add("mem[7] = 101");
+        instructions.add("mem[8] = 0");
+
+        Day14 d = new Day14();
+        long partOneAnswer = d.solvePartOne(instructions);
+
+        Assert.assertEquals(165, partOneAnswer);
+    }
+
+    /**
+     * Test Day 14 part one with the real input (and the correct answer)
+     */
+    @Test
+    public void testRealPartOne() {
+        List<String> instructions = ProblemLoader.loadProblemIntoStringArray(2020, 14);
+        Day14 d = new Day14();
+        long partOneAnswer = d.solvePartOne(instructions);
+
+        Assert.assertEquals(15514035145260L, partOneAnswer);
+    }
+
+    /**
+     * Test Day 14 part two with the example input (with the known result)
+     */
+    @Test
+    public void testExampleInputPartTwo() {
+        List<String> instructions = new ArrayList<>();
+        instructions.add("mask = 000000000000000000000000000000X1001X");
+        instructions.add("mem[42] = 100");
+        instructions.add("mask = 00000000000000000000000000000000X0XX");
+        instructions.add("mem[26] = 1");
+
+        Day14 d = new Day14();
+        long partTwoAnswer = d.solvePartTwo(instructions);
+
+        Assert.assertEquals(208, partTwoAnswer);
+    }
+
+    /**
+     * Test Day 14 part two with the real input (with the correct answer)
+     */
+    @Test
+    public void testRealPartTwo() {
+        List<String> instructions = ProblemLoader.loadProblemIntoStringArray(2020, 14);
+        Day14 d = new Day14();
+        long partTwoAnswer = d.solvePartTwo(instructions);
+
+        Assert.assertEquals(3926790061594L, partTwoAnswer);
+    }
+
+}
