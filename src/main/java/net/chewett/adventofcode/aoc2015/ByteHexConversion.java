5feae8c9d2525c4f7aaefd84e25d07b45abc26e7
Christopher Hewett <chewett@hotmail.co.uk>
2021-12-18 16:37:06 +0000

    Added a simple conversion from HEX to binary

--- src/main/java/net/chewett/adventofcode/aoc2015/ByteHexConversion.java
+++ src/main/java/net/chewett/adventofcode/aoc2015/ByteHexConversion.java
@@ -21,4 +21,30 @@ public class ByteHexConversion {
         return hexString.toString();
     }
 
+    /**
+     * Really simple function to convert a string of hex into a binary string
+     * @param hexString A string representing a hex number
+     * @return A string of binary
+     */
+    public static String convertHexStringToBinaryString(String hexString) {
+        String newHexString = hexString;
+        newHexString = newHexString.replaceAll("0", "0000");
+        newHexString = newHexString.replaceAll("1", "0001");
+        newHexString = newHexString.replaceAll("2", "0010");
+        newHexString = newHexString.replaceAll("3", "0011");
+        newHexString = newHexString.replaceAll("4", "0100");
+        newHexString = newHexString.replaceAll("5", "0101");
+        newHexString = newHexString.replaceAll("6", "0110");
+        newHexString = newHexString.replaceAll("7", "0111");
+        newHexString = newHexString.replaceAll("8", "1000");
+        newHexString = newHexString.replaceAll("9", "1001");
+        newHexString = newHexString.replaceAll("A", "1010");
+        newHexString = newHexString.replaceAll("B", "1011");
+        newHexString = newHexString.replaceAll("C", "1100");
+        newHexString = newHexString.replaceAll("D", "1101");
+        newHexString = newHexString.replaceAll("E", "1110");
+        newHexString = newHexString.replaceAll("F", "1111");
+        return newHexString;
+    }
+
 }
