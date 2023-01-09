6eb805c4e61a5bfe398db6ac865664914c240903
Christopher Hewett <chewett@hotmail.co.uk>
2023-01-09 17:44:30 +0000

    Added in (probably) the right intersection code

--- src/main/java/net/chewett/adventofcode/datastructures/Cuboid.java
+++ src/main/java/net/chewett/adventofcode/datastructures/Cuboid.java
@@ -10,17 +10,22 @@ public class Cuboid {
     private int z2;
 
     public Cuboid(int x, int x2, int y, int y2, int z, int z2) {
-        this.x = x;
-        this.x2 = x2;
-        this.y = y;
-        this.y2 = y2;
-        this.z = z;
-        this.z2 = z2;
+        //Min/max forces an ordering in the event this cuboid is oddly dimensioned
+        this.x = Math.min(x, x2);
+        this.x2 = Math.max(x, x2);
+        this.y = Math.min(y, y2);
+        this.y2 = Math.max(y, y2);
+        this.z = Math.min(z, z2);
+        this.z2 =Math.max(z, z2);
     }
 
     public boolean intersects(Cuboid c) {
-        return false;
+        //Insection is that
+        //For every axis (X, Y, Z)
+        //  - MinX (defined as x) is less than or equal to Max X of C (defined as x2)
+        //  - MinX of C (defined as x) is less than or equal to Max X (defined as x2)
 
+        return (this.x <= c.x2 && c.x <= this.x2) && (this.y <= c.y2 && c.y <= this.y2) && (this.z <= c.z2 && c.z <= this.z2);
     }
 
     public long getContainedArea() {
