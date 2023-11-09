b9be8c8e7feebcd45256bde0051c021e245853aa
Christopher Hewett <chewett@hotmail.co.uk>
2023-11-09 11:29:20 +0000

    Refactor this into a nicer way of running the code

--- src/main/java/net/chewett/adventofcode/aoc2021/problems/Day23.java
+++ src/main/java/net/chewett/adventofcode/aoc2021/problems/Day23.java
@@ -9,18 +9,20 @@ import java.util.*;
  */
 public class Day23 {
 
-
-    public long solvePartOne(String exampleInput) {
-
-        Day23State state = new Day23State(exampleInput, 0);
+    private int solveWithNormal(String exampleInput, boolean extended) {
+        Day23State state = new Day23State(exampleInput, 0, extended);
 
         Set<String> foundStates = new HashSet<>();
         Map<String, Integer> stateEnergyMin = new HashMap<>();
 
         PriorityQueue<Day23State> queue = new PriorityQueue<>();
         queue.add(state);
+
+        state.printThis();
+
+        int bestState = Integer.MAX_VALUE;
         Day23State foundEnd = null;
-        while(foundEnd == null && !queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             Day23State checkState = queue.poll();
             String thisStateAsString = checkState.getState();
 
@@ -35,34 +37,34 @@ public class Day23 {
 
             foundStates.add(thisStateAsString);
             stateEnergyMin.put(thisStateAsString, checkState.getEnergy());
-            //System.out.println(queue.size());
-            //checkState.printThis();
             if(checkState.isFinalState()) {
-                foundEnd = checkState;
-            }else{
-                List<Day23State> newStates = checkState.getNewStates();
-                for(Day23State s : newStates) {
-                    //s.printThis();
+                if(checkState.getEnergy() < bestState) {
+                    foundEnd = checkState;
+                    bestState = checkState.getEnergy();
                 }
 
+            }else{
+                List<Day23State> newStates = checkState.getNewStates();
                 queue.addAll(newStates);
             }
         }
 
-        //foundEnd.printThis();
+        foundEnd.printThis();
+        return foundEnd.getEnergy();
+    }
 
-        //Remove 2 from the solution because something is funky...
-        return foundEnd.getEnergy() - 2;
 
+    public long solvePartOne(String exampleInput) {
+        return this.solveWithNormal(exampleInput, false);
     }
 
     public long solvePartTwo(String exampleInput) {
-        return -1;
+        return this.solveWithNormal(exampleInput, true);
     }
 
     public static void main(String[] args) {
         String exampleInput = "00BA0CD0BC0DA00";
-        exampleInput = "00BC0DD0CB0AA00";
+        //exampleInput = "00BC0DD0CB0AA00";
 
         Day23 d = new Day23();
         System.out.println(" " + d.solvePartOne(exampleInput));
