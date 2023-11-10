f3f84446a2572994efb3b62bfb4891842ab0b6fd
Christopher Hewett <chewett@hotmail.co.uk>
2023-11-10 22:58:09 +0000

    Solve Day 25 and finish advent of code 2021

---
+++ src/main/java/net/chewett/adventofcode/aoc2021/problems/Day25.java
@@ -0,0 +1,371 @@
+package net.chewett.adventofcode.aoc2021.problems;
+
+import net.chewett.adventofcode.datastructures.Discrete2DPositionGrid;
+import net.chewett.adventofcode.helpers.ProblemLoader;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * --- Day 25: Sea Cucumber ---
+ * This is it: the bottom of the ocean trench, the last place the sleigh keys could be. Your submarine's experimental
+ * antenna still isn't boosted enough to detect the keys, but they must be here. All you need to do is reach the
+ * seafloor and find them.
+ *
+ * At least, you'd touch down on the seafloor if you could; unfortunately, it's completely covered by two large herds
+ * of sea cucumbers, and there isn't an open space large enough for your submarine.
+ *
+ * You suspect that the Elves must have done this before, because just then you discover the phone number of a deep-sea
+ * marine biologist on a handwritten note taped to the wall of the submarine's cockpit.
+ *
+ * "Sea cucumbers? Yeah, they're probably hunting for food. But don't worry, they're predictable critters: they move in
+ * perfectly straight lines, only moving forward when there's space to do so. They're actually quite polite!"
+ *
+ * You explain that you'd like to predict when you could land your submarine.
+ *
+ * "Oh that's easy, they'll eventually pile up and leave enough space for-- wait, did you say submarine? And the only
+ * place with that many sea cucumbers would be at the very bottom of the Mariana--" You hang up the phone.
+ *
+ * There are two herds of sea cucumbers sharing the same region; one always moves east (>), while the other always
+ * moves south (v). Each location can contain at most one sea cucumber; the remaining locations are empty (.). The
+ * submarine helpfully generates a map of the situation (your puzzle input). For example:
+ *
+ * v...>>.vv>
+ * .vv>>.vv..
+ * >>.>v>...v
+ * >>v>>.>.v.
+ * v>v.vv.v..
+ * >.>>..v...
+ * .vv..>.>v.
+ * v.v..>>v.v
+ * ....v..v.>
+ * Every step, the sea cucumbers in the east-facing herd attempt to move forward one location, then the sea cucumbers in the south-facing herd attempt to move forward one location. When a herd moves forward, every sea cucumber in the herd first simultaneously considers whether there is a sea cucumber in the adjacent location it's facing (even another sea cucumber facing the same direction), and then every sea cucumber facing an empty location simultaneously moves into that location.
+ *
+ * So, in a situation like this:
+ *
+ * ...>>>>>...
+ * After one step, only the rightmost sea cucumber would have moved:
+ *
+ * ...>>>>.>..
+ * After the next step, two sea cucumbers move:
+ *
+ * ...>>>.>.>.
+ * During a single step, the east-facing herd moves first, then the south-facing herd moves. So, given this situation:
+ *
+ * ..........
+ * .>v....v..
+ * .......>..
+ * ..........
+ * After a single step, of the sea cucumbers on the left, only the south-facing sea cucumber has moved (as it wasn't
+ * out of the way in time for the east-facing cucumber on the left to move), but both sea cucumbers on the right have
+ * moved (as the east-facing sea cucumber moved out of the way of the south-facing sea cucumber):
+ *
+ * ..........
+ * .>........
+ * ..v....v>.
+ * ..........
+ * Due to strong water currents in the area, sea cucumbers that move off the right edge of the map appear on the left
+ * edge, and sea cucumbers that move off the bottom edge of the map appear on the top edge. Sea cucumbers always check
+ * whether their destination location is empty before moving, even if that destination is on the opposite side of the
+ * map:
+ *
+ * Initial state:
+ * ...>...
+ * .......
+ * ......>
+ * v.....>
+ * ......>
+ * .......
+ * ..vvv..
+ *
+ * After 1 step:
+ * ..vv>..
+ * .......
+ * >......
+ * v.....>
+ * >......
+ * .......
+ * ....v..
+ *
+ * After 2 steps:
+ * ....v>.
+ * ..vv...
+ * .>.....
+ * ......>
+ * v>.....
+ * .......
+ * .......
+ *
+ * After 3 steps:
+ * ......>
+ * ..v.v..
+ * ..>v...
+ * >......
+ * ..>....
+ * v......
+ * .......
+ *
+ * After 4 steps:
+ * >......
+ * ..v....
+ * ..>.v..
+ * .>.v...
+ * ...>...
+ * .......
+ * v......
+ * To find a safe place to land your submarine, the sea cucumbers need to stop moving. Again consider the first example:
+ *
+ * Initial state:
+ * v...>>.vv>
+ * .vv>>.vv..
+ * >>.>v>...v
+ * >>v>>.>.v.
+ * v>v.vv.v..
+ * >.>>..v...
+ * .vv..>.>v.
+ * v.v..>>v.v
+ * ....v..v.>
+ *
+ * After 1 step:
+ * ....>.>v.>
+ * v.v>.>v.v.
+ * >v>>..>v..
+ * >>v>v>.>.v
+ * .>v.v...v.
+ * v>>.>vvv..
+ * ..v...>>..
+ * vv...>>vv.
+ * >.v.v..v.v
+ *
+ * After 2 steps:
+ * >.v.v>>..v
+ * v.v.>>vv..
+ * >v>.>.>.v.
+ * >>v>v.>v>.
+ * .>..v....v
+ * .>v>>.v.v.
+ * v....v>v>.
+ * .vv..>>v..
+ * v>.....vv.
+ *
+ * After 3 steps:
+ * v>v.v>.>v.
+ * v...>>.v.v
+ * >vv>.>v>..
+ * >>v>v.>.v>
+ * ..>....v..
+ * .>.>v>v..v
+ * ..v..v>vv>
+ * v.v..>>v..
+ * .v>....v..
+ *
+ * After 4 steps:
+ * v>..v.>>..
+ * v.v.>.>.v.
+ * >vv.>>.v>v
+ * >>.>..v>.>
+ * ..v>v...v.
+ * ..>>.>vv..
+ * >.v.vv>v.v
+ * .....>>vv.
+ * vvv>...v..
+ *
+ * After 5 steps:
+ * vv>...>v>.
+ * v.v.v>.>v.
+ * >.v.>.>.>v
+ * >v>.>..v>>
+ * ..v>v.v...
+ * ..>.>>vvv.
+ * .>...v>v..
+ * ..v.v>>v.v
+ * v.v.>...v.
+ *
+ * ...
+ *
+ * After 10 steps:
+ * ..>..>>vv.
+ * v.....>>.v
+ * ..v.v>>>v>
+ * v>.>v.>>>.
+ * ..v>v.vv.v
+ * .v.>>>.v..
+ * v.v..>v>..
+ * ..v...>v.>
+ * .vv..v>vv.
+ *
+ * ...
+ *
+ * After 20 steps:
+ * v>.....>>.
+ * >vv>.....v
+ * .>v>v.vv>>
+ * v>>>v.>v.>
+ * ....vv>v..
+ * .v.>>>vvv.
+ * ..v..>>vv.
+ * v.v...>>.v
+ * ..v.....v>
+ *
+ * ...
+ *
+ * After 30 steps:
+ * .vv.v..>>>
+ * v>...v...>
+ * >.v>.>vv.>
+ * >v>.>.>v.>
+ * .>..v.vv..
+ * ..v>..>>v.
+ * ....v>..>v
+ * v.v...>vv>
+ * v.v...>vvv
+ *
+ * ...
+ *
+ * After 40 steps:
+ * >>v>v..v..
+ * ..>>v..vv.
+ * ..>>>v.>.v
+ * ..>>>>vvv>
+ * v.....>...
+ * v.v...>v>>
+ * >vv.....v>
+ * .>v...v.>v
+ * vvv.v..v.>
+ *
+ * ...
+ *
+ * After 50 steps:
+ * ..>>v>vv.v
+ * ..v.>>vv..
+ * v.>>v>>v..
+ * ..>>>>>vv.
+ * vvv....>vv
+ * ..v....>>>
+ * v>.......>
+ * .vv>....v>
+ * .>v.vv.v..
+ *
+ * ...
+ *
+ * After 55 steps:
+ * ..>>v>vv..
+ * ..v.>>vv..
+ * ..>>v>>vv.
+ * ..>>>>>vv.
+ * v......>vv
+ * v>v....>>v
+ * vvv...>..>
+ * >vv.....>.
+ * .>v.vv.v..
+ *
+ * After 56 steps:
+ * ..>>v>vv..
+ * ..v.>>vv..
+ * ..>>v>>vv.
+ * ..>>>>>vv.
+ * v......>vv
+ * v>v....>>v
+ * vvv....>.>
+ * >vv......>
+ * .>v.vv.v..
+ *
+ * After 57 steps:
+ * ..>>v>vv..
+ * ..v.>>vv..
+ * ..>>v>>vv.
+ * ..>>>>>vv.
+ * v......>vv
+ * v>v....>>v
+ * vvv.....>>
+ * >vv......>
+ * .>v.vv.v..
+ *
+ * After 58 steps:
+ * ..>>v>vv..
+ * ..v.>>vv..
+ * ..>>v>>vv.
+ * ..>>>>>vv.
+ * v......>vv
+ * v>v....>>v
+ * vvv.....>>
+ * >vv......>
+ * .>v.vv.v..
+ * In this example, the sea cucumbers stop moving after 58 steps.
+ *
+ * Find somewhere safe to land your submarine. What is the first step on which no sea cucumbers move?
+ */
+public class Day25 {
+
+
+    /**
+     * Relatively simple permutation solver today
+     * @param grid The grid of data
+     * @return The number of steps to wait until the sea cucumbers have stopped
+     */
+    public long solvePartOne(Discrete2DPositionGrid<Character> grid) {
+
+        //Simple while loop to keep running til its finished
+        boolean somethingMoved = true;
+        int step = 1;
+        while(somethingMoved) {
+            somethingMoved = false;
+
+            Discrete2DPositionGrid<Character> newGrid = new Discrete2DPositionGrid<>('.');
+
+            //Loop over all the east movers, and move them if they can
+            List<Point> allEast = grid.getPositionsOfValue('>');
+            for(Point eastCucumber : allEast) {
+
+                Point newEastCucumber = new Point(eastCucumber);
+                newEastCucumber.x += 1;
+                if(newEastCucumber.x > grid.getMaxX()) {
+                    newEastCucumber.x = 0;
+                }
+
+                //If there is a space there now we can move into it
+                if(grid.getValueAtPosition(newEastCucumber) == '.') {
+                    newGrid.setValueAtPosition(newEastCucumber, '>');
+                    somethingMoved = true;
+                }else{
+                    newGrid.setValueAtPosition(eastCucumber, '>');
+                }
+            }
+
+            //Same again but for south cucumbers
+            List<Point> allSouth = grid.getPositionsOfValue('v');
+            for(Point southCucumber : allSouth) {
+
+                Point newSouthCucumber = new Point(southCucumber);
+                newSouthCucumber.y += 1;
+                if(newSouthCucumber.y > grid.getMaxY()) {
+                    newSouthCucumber.y = 0;
+                }
+
+                //We need to check both the current grid for v's and the new grid for dots, but thats the only trick here
+                if(grid.getValueAtPosition(newSouthCucumber) != 'v' && newGrid.getValueAtPosition(newSouthCucumber) == '.') {
+                    newGrid.setValueAtPosition(newSouthCucumber, 'v');
+                    somethingMoved = true;
+                }else{
+                    newGrid.setValueAtPosition(southCucumber, 'v');
+                }
+            }
+
+            if(somethingMoved) {
+                step++;
+                grid = newGrid;
+            }
+        }
+
+        return step;
+    }
+
+    public static void main(String[] args) {
+        Discrete2DPositionGrid<Character> grid = ProblemLoader.loadProblemIntoDiscrete2DPositionGridCharacter(2021, 25);
+
+        Day25 d = new Day25();
+        System.out.println("The number of steps to wait for the sea cucumbers to finish is " + d.solvePartOne(grid));
+
+    }
+
+}
---
+++ src/test/java/net/chewett/adventofcode/aoc2021/problems/Day25Test.java
@@ -0,0 +1,56 @@
+package net.chewett.adventofcode.aoc2021.problems;
+
+import net.chewett.adventofcode.datastructures.Discrete2DPositionGrid;
+import net.chewett.adventofcode.helpers.FormatConversion;
+import net.chewett.adventofcode.helpers.ProblemLoader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day25Test {
+
+    /**
+     * Function to get the example inputs
+     * @return The example input
+     */
+    public Discrete2DPositionGrid<Character> getExampleInput() {
+        List<String> seaCucumbers = new ArrayList<>();
+
+        seaCucumbers.add("v...>>.vv>");
+        seaCucumbers.add(".vv>>.vv..");
+        seaCucumbers.add(">>.>v>...v");
+        seaCucumbers.add(">>v>>.>.v.");
+        seaCucumbers.add("v>v.vv.v..");
+        seaCucumbers.add(">.>>..v...");
+        seaCucumbers.add(".vv..>.>v.");
+        seaCucumbers.add("v.v..>>v.v");
+        seaCucumbers.add("....v..v.>");
+
+        List<List<Character>> octopiArray = FormatConversion.convertStringArrayToCharListList(seaCucumbers);
+        return FormatConversion.convertCharArrayIntoDiscrete2DPositionGridCharacter(octopiArray);
+    }
+
+
+    /**
+     * Test Day 25 part one with the example input (with the known result)
+     */
+    @Test
+    public void testExampleInputPartOne() {
+        Day25 d = new Day25();
+        Assertions.assertEquals(58, d.solvePartOne(this.getExampleInput()));
+    }
+
+
+    /**
+     * Test Day 25 part one with the real input (and the correct answer)
+     */
+    @Test
+    public void testRealPartOne() {
+        Discrete2DPositionGrid<Character> input = ProblemLoader.loadProblemIntoDiscrete2DPositionGridCharacter(2021, 25);
+        Day25 d = new Day25();
+        Assertions.assertEquals(549, d.solvePartOne(input));
+    }
+
+}
