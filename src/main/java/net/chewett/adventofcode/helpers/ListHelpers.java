ffeee41897a5a08f626d8389c859fc1ca68d25fd
Christopher Hewett <chewett@hotmail.co.uk>
2024-12-13 19:54:55 +0000

    Added a simple counting counting list helper

--- src/main/java/net/chewett/adventofcode/helpers/ListHelpers.java
+++ src/main/java/net/chewett/adventofcode/helpers/ListHelpers.java
@@ -1,6 +1,8 @@
 package net.chewett.adventofcode.helpers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListHelpers {
 
@@ -15,6 +17,15 @@ public class ListHelpers {
             maxInt = Math.max(maxInt, i);
         }
         return maxInt;
+    }
+
+    public static Map<String, Integer> countOccurencesInlist(List<String> list) {
+        Map<String, Integer> map = new HashMap();
+        for(String s : list) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        return map;
     }
 
 }
