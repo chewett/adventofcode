61a4ebf0c9c3d04d52f944713c6d05009aa64125
Christopher Hewett <chewett@hotmail.co.uk>
2024-12-30 16:13:27 +0000

    Added some documentation tweaks and moved to using longs because while longs are not needed here, they typically are

--- src/main/java/net/chewett/adventofcode/aoc2019/spaceship/Spaceship.java
+++ src/main/java/net/chewett/adventofcode/aoc2019/spaceship/Spaceship.java
@@ -3,16 +3,28 @@ package net.chewett.adventofcode.aoc2019.spaceship;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Helper method for Day 1 calculation of fuel weights
+ * THis holds a list of spaceship modules which each need an amount of fuel to send them to space
+ */
 public class Spaceship {
 
     private List<SpaceshipModule> modules = new ArrayList<>();
 
+    /**
+     * Adds a module to the list of modules stored in the ship
+     * @param module
+     */
     public void addModule(SpaceshipModule module) {
         this.modules.add(module);
     }
 
-    public int getFuelForModules() {
-        int totalFuel = 0;
+    /**
+     * Gets the fuel needed for the modules (ignoring the fuel needed for the fuel) on the ship
+     * @return THe amount of fuel to get the modules to space
+     */
+    public long getFuelForModules() {
+        long totalFuel = 0;
         for(SpaceshipModule sm : this.modules) {
             totalFuel += sm.getFuelNeededToTransportModule();
         }
@@ -20,8 +32,13 @@ public class Spaceship {
         return totalFuel;
     }
 
-    public int getFuelForModulesCountingFuelMass() {
-        int totalFuel = 0;
+    /**
+     * Gets the fuel needed to actually get the modules into space, by taking into account the extra fuel needed for the
+     * fuel
+     * @return The amount of fuel needed to get the modules and fuel into space
+     */
+    public long getFuelForModulesCountingFuelMass() {
+        long totalFuel = 0;
         for(SpaceshipModule sm : this.modules) {
             totalFuel += sm.getFuelNeededToTransportModuleIncludingFuelMass();
         }
--- src/main/java/net/chewett/adventofcode/aoc2019/spaceship/SpaceshipModule.java
+++ src/main/java/net/chewett/adventofcode/aoc2019/spaceship/SpaceshipModule.java
@@ -1,24 +1,41 @@
 package net.chewett.adventofcode.aoc2019.spaceship;
 
+/**
+ * A spaceship module to be added to a Spaceship for Day 1 2019. This is used to calculate the fuel requirements
+ */
 public class SpaceshipModule {
 
-    private int mass;
+    private long mass;
 
-    public SpaceshipModule(int mass) {
+    public SpaceshipModule(long mass) {
         this.mass = mass;
     }
 
-    private int calculateFuelForMass(int mass) {
-        return (int)(Math.floor(mass / 3.0) - 2);
+    /**
+     * Works out the amount of fuel needed for the given mass
+     * @param mass Mass to calculate for
+     * @return The total fuel needed for the given mass
+     */
+    private long calculateFuelForMass(long mass) {
+        return (long)(Math.floor(mass / 3.0) - 2);
     }
 
-    public int getFuelNeededToTransportModule() {
+    /**
+     * Gets the fuel needed to move this transport module (does not account for own fuels weight)
+     * @return Fuel needed to move this transport module
+     */
+    public long getFuelNeededToTransportModule() {
         return this.calculateFuelForMass(this.mass);
     }
 
-    public int getFuelNeededToTransportModuleIncludingFuelMass() {
-        int totalFuelCosts = this.calculateFuelForMass(this.mass);
-        int newMass = totalFuelCosts;
+    /**
+     * The correct calculation where the fuel's mass is considered iteratively and updated until we have enough fuel
+     * to move the fuel
+     * @return The amount of fuel needed to move the fuel and module
+     */
+    public long getFuelNeededToTransportModuleIncludingFuelMass() {
+        long totalFuelCosts = this.calculateFuelForMass(this.mass);
+        long newMass = totalFuelCosts;
         while(newMass > 0) {
             newMass = this.calculateFuelForMass(newMass);
             if(newMass > 0) {
